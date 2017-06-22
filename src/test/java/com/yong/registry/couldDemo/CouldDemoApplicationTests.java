99a136fcd97b147b2da878226640cb5f9050bcdc
LiangYong <yong.a.liang@accenture.com>
2017-06-22 10:20:16 +0800

    first commit

---
+++ src/test/java/com/yong/registry/couldDemo/CouldDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yong.registry.couldDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CouldDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
